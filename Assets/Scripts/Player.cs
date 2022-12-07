1d6c28d1d9b0414cafafcddeae00b7aff893f9d8
dbowton <80086095+dbowton@users.noreply.github.com>
2022-12-07 07:44:20 -0700

    Color Customization

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,14 +18,11 @@ public class Player : MonoBehaviour
 
     private void Start()
     {        
-        vehicleMenu.enableAutoSizing = true;
-        vehicleMenu.enabled = false;
+        menu = menuObject.GetComponent<VehicleSpawnMenu>();
     }
 
     [SerializeField] List<GameObject> vehicles = new List<GameObject>();
     int vehicleIndex = 0;
-
-    [SerializeField] TMPro.TMP_Text vehicleMenu;
 
     bool readyToSwitchIndex = false;
 
@@ -35,8 +32,267 @@ public class Player : MonoBehaviour
 
     public bool canOpenDoor = true;
 
+    public bool inMenu = false;
+    public GameObject menuObject;
+    private VehicleSpawnMenu menu;
+
+    private int menuState = 0;
+
+
+    private int selectedType = 0;
+    private int selectedSlider = 0;
+
+    [SerializeField] Material bodyMat;
+    [SerializeField] Material accentMat;
+    [SerializeField] Material tireMat;
+
+    private bool prevPressed = false;
+
     private void Update()
     {
+        if (!inMenu && car == null && ControllerManager.rightInput.GetControllerPressed(VRButton.primaryButton, out bool startMenu) && startMenu)
+        {
+            prevPressed = true;
+            inMenu = true;
+
+            menu.spawnObject.SetActive(true);
+            menu.customizeObject.SetActive(false);
+            menu.colorObject.SetActive(false);
+
+            menuState = 0;
+
+            menu.vehicleName.text = vehicles[vehicleIndex].name + "\n" + (vehicleIndex + 1) + " / " + vehicles.Count;
+            return;
+        }
+
+        if (inMenu)
+        {
+            if (prevPressed)
+            {
+                prevPressed = (ControllerManager.rightInput.GetControllerPressed(VRButton.primaryButton, out bool primaryPressed) && primaryPressed) ||
+                    (ControllerManager.rightInput.GetControllerPressed(VRButton.secondaryButton, out bool secondaryPressed) && secondaryPressed);
+
+                return;
+            }
+
+            switch (menuState)
+            {
+                case 0: // vehicleSpawn
+                    {
+                        if (ControllerManager.rightInput.GetControllerPressed(VRButton.triggerButton, out bool spawnVehcile) && spawnVehcile)
+                        {
+                            if (spawnedVehicle) Destroy(spawnedVehicle.gameObject);
+
+                            Transform cameraT = Camera.main.transform;
+
+                            Vector3 adjustedForward = cameraT.forward - Vector3.up * cameraT.forward.y;
+                            Vector3 spawnPoint = cameraT.position + adjustedForward * spawnDistance;
+                            Vector3 spawnRot = Vector3.up;
+
+                            if (Physics.Raycast(cameraT.position, adjustedForward, out RaycastHit hitInfo, spawnDistance))
+                            {
+                                spawnPoint = hitInfo.point + Vector3.up * spawnDistance;
+                                spawnRot = hitInfo.normal;
+                            }
+
+                            spawnedVehicle = Instantiate(vehicles[vehicleIndex], spawnPoint, Quaternion.identity).GetComponent<Vehicle>();
+                            spawnedVehicle.gameObject.transform.LookAt(spawnedVehicle.transform.position + (spawnedVehicle.transform.position - transform.position), spawnRot);
+
+                            inMenu = false;
+                            menu.spawnObject.SetActive(false);
+                            return;
+                        }
+
+                        if (ControllerManager.rightInput.GetControllerPressed(VRButton.primary2DAxis, out Vector2 choice))
+                        {
+                            if (!readyToSwitchIndex && Mathf.Abs(choice.x) <= 0.15f)
+                            {
+                                readyToSwitchIndex = true;
+                            }
+                            else if (readyToSwitchIndex && Mathf.Abs(choice.x) > 0.9f)
+                            {
+                                readyToSwitchIndex = false;
+                                if (choice.x > 0.9f) vehicleIndex++;
+                                if (choice.x < -0.9f) vehicleIndex--;
+
+                                if (vehicleIndex < 0) vehicleIndex = vehicles.Count - 1;
+                                if (vehicleIndex >= vehicles.Count) vehicleIndex = 0;
+
+                                menu.vehicleName.text = "< " + vehicles[vehicleIndex].name + " >\n" + (vehicleIndex + 1) + " / " + vehicles.Count;
+                                return;
+                            }
+                        }
+
+                        if (ControllerManager.rightInput.GetControllerPressed(VRButton.primaryButton, out bool cusomize) && cusomize)
+                        {
+                            prevPressed = true;
+
+                            menuState = 1;
+
+                            menu.spawnObject.SetActive(false);
+                            menu.customizeObject.SetActive(true);
+                            return;
+                        }
+
+                        if (ControllerManager.rightInput.GetControllerPressed(VRButton.secondaryButton, out bool back) && back)
+                        {
+                            prevPressed = true;
+
+                            inMenu = false;
+                            menu.spawnObject.SetActive(false);
+                            return;
+                        }
+                    }
+                    break;
+                case 1: // selectType
+                    {
+                        menu.bodyText.color = (selectedType == 0) ? Color.black : Color.white;
+                        menu.accentText.color = (selectedType == 1) ? Color.black : Color.white;
+                        menu.tireText.color = (selectedType == 2) ? Color.black : Color.white;
+
+                        if (ControllerManager.rightInput.GetControllerPressed(VRButton.primary2DAxis, out Vector2 changeType))
+                        {
+                            if (!readyToSwitchIndex && Mathf.Abs(changeType.y) <= 0.15f)
+                            {
+                                readyToSwitchIndex = true;
+                            }
+                            else if (readyToSwitchIndex && Mathf.Abs(changeType.y) > 0.9f)
+                            {
+                                readyToSwitchIndex = false;
+                                if (changeType.y > 0.9f) selectedType--;
+                                else if (changeType.y < -0.9f) selectedType++;
+
+                                if (selectedType < 0) selectedType = 2;
+                                else if (selectedType > 2) selectedType = 0;
+
+                                return;
+                            }
+                        }
+
+                        if (ControllerManager.rightInput.GetControllerPressed(VRButton.primaryButton, out bool selectType) && selectType)
+                        {
+                            prevPressed = true;
+                            menuState = 2;
+
+                            menu.customizeObject.SetActive(false);
+                            menu.colorObject.SetActive(true);
+
+                            selectedSlider = 0;
+
+                            return;
+                        }
+
+                        if (ControllerManager.rightInput.GetControllerPressed(VRButton.secondaryButton, out bool back) && back)
+                        {
+                            prevPressed = true;
+                            menuState = 0;
+
+                            menu.spawnObject.SetActive(true);
+                            menu.customizeObject.SetActive(false);
+
+                            return;
+                        }
+
+                    }
+                    break;
+                case 2: // customizeColor
+                    {
+                        Material mat = bodyMat;
+
+                        if (selectedType == 0) mat = bodyMat;
+                        if (selectedType == 1) mat = accentMat;
+                        if (selectedType == 2) mat = tireMat;
+
+                        menu.rSlider.value = mat.color.r;
+                        menu.gSlider.value = mat.color.g;
+                        menu.bSlider.value = mat.color.b;
+
+                        menu.sliderColor.material = mat;
+                        menu.colorType.text = (selectedType == 0) ? "Body Color" : (selectedType == 1) ? "Accent Color" : "Tire Color";
+
+                        UnityEngine.UI.ColorBlock block = menu.rSlider.colors;
+                        block.normalColor = (selectedSlider == 0) ? Color.black : Color.white;
+
+                        menu.rSlider.colors = block;
+
+                        block = menu.gSlider.colors;
+                        block.normalColor = (selectedSlider == 1) ? Color.black : Color.white;
+
+                        menu.gSlider.colors = block;
+
+                        block = menu.bSlider.colors;
+                        block.normalColor = (selectedSlider == 2) ? Color.black : Color.white;
+
+                        menu.bSlider.colors = block;
+
+                        if (ControllerManager.rightInput.GetControllerPressed(VRButton.primary2DAxis, out Vector2 movement))
+                        {
+                            if (!readyToSwitchIndex && movement.magnitude <= 0.15f)
+                            {
+                                readyToSwitchIndex = true;
+                            }
+                            else if (readyToSwitchIndex && movement.magnitude > 0.9f)
+                            {
+                                readyToSwitchIndex = false;
+
+                                if (movement.y > 0.9f)
+                                {
+                                    selectedSlider--;
+                                    if (selectedSlider < 0) selectedSlider = 2;
+
+                                    return;
+                                }
+                                if (movement.y < -0.9f)
+                                {
+                                    selectedSlider++;
+                                    if (selectedSlider > 2) selectedSlider = 0;
+
+                                    return;
+                                }
+
+                                if (movement.x > 0.9f || movement.x < -0.9f)
+                                {
+                                    Color newColor = mat.color;
+
+                                    float change = (movement.x > 0.9f) ? 0.1f : -0.1f;
+
+                                    if (selectedSlider == 0) newColor.r = Mathf.Max(0, Mathf.Min(1, mat.color.r + change));
+                                    if (selectedSlider == 1) newColor.g = Mathf.Max(0, Mathf.Min(1, mat.color.g + change));
+                                    if (selectedSlider == 2) newColor.b = Mathf.Max(0, Mathf.Min(1, mat.color.b + change));
+
+                                    mat.color = newColor;
+                                    return;
+                                }
+
+
+                                return;
+                            }
+                        }
+
+                        if (ControllerManager.rightInput.GetControllerPressed(VRButton.secondaryButton, out bool back) && back)
+                        {
+                            prevPressed = true;
+                            menuState = 1;
+
+                            menu.colorObject.SetActive(false);
+                            menu.customizeObject.SetActive(true);
+
+                            return;
+                        }
+
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+
+            return;
+        }
+        else prevPressed = false;
+
+
+
         if (car == null)
         {
             transform.localEulerAngles = Vector3.up * transform.localEulerAngles.y;
@@ -55,59 +311,6 @@ public class Player : MonoBehaviour
             else
             {
                 controller.SimpleMove(Vector3.down);
-                if (Physics.Raycast(ControllerManager.RightHandPos, ControllerManager.rightController.controllerObject.transform.forward, out _, 4)) { }
-            }
-
-            if(!vehicleMenu.enabled)
-            {
-                if(ControllerManager.rightInput.GetControllerPressed(VRButton.primaryButton, out bool pressed) && pressed)
-                {
-                    vehicleMenu.enabled = true;
-                    vehicleMenu.text = "< " + vehicles[vehicleIndex].name + " >\n" + (vehicleIndex + 1) + " / " + vehicles.Count;
-                }
-            }
-            else
-            {
-                if (ControllerManager.rightInput.GetControllerPressed(VRButton.primary2DAxis, out Vector2 choice))
-                {
-                    if (!readyToSwitchIndex && Mathf.Abs(choice.x) <= 0.15f)
-                    {
-                        readyToSwitchIndex = true;
-                    }
-                    else if (readyToSwitchIndex && Mathf.Abs(choice.x) > 0.9f)
-                    {
-                        readyToSwitchIndex = false;
-                        if (choice.x > 0.9f) vehicleIndex++;
-                        if (choice.x < -0.9f) vehicleIndex--;
-
-                        if (vehicleIndex < 0) vehicleIndex = vehicles.Count - 1;
-                        if (vehicleIndex >= vehicles.Count) vehicleIndex = 0;
-
-                        vehicleMenu.text = "< " + vehicles[vehicleIndex].name + " >\n" + (vehicleIndex + 1) + " / " + vehicles.Count;
-                    }
-                }
-
-                if (ControllerManager.rightInput.GetControllerPressed(VRButton.triggerButton, out bool pressed) && pressed)
-                {
-                    if (spawnedVehicle) Destroy(spawnedVehicle.gameObject);
-
-                    Transform cameraT = Camera.main.transform;
-
-                    Vector3 adjustedForward = cameraT.forward - Vector3.up * cameraT.forward.y;
-                    Vector3 spawnPoint = cameraT.position + adjustedForward * spawnDistance;
-                    Vector3 spawnRot = Vector3.up;
-
-                    if(Physics.Raycast(cameraT.position, adjustedForward, out RaycastHit hitInfo, spawnDistance))
-                    {
-                        spawnPoint = hitInfo.point + Vector3.up * spawnDistance;
-                        spawnRot = hitInfo.normal;
-                    }
-
-                    spawnedVehicle = Instantiate(vehicles[vehicleIndex], spawnPoint, Quaternion.identity).GetComponent<Vehicle>();
-                    spawnedVehicle.gameObject.transform.LookAt(spawnedVehicle.transform.position + (spawnedVehicle.transform.position - transform.position), spawnRot);
-
-                    vehicleMenu.enabled = false;
-                }
             }
         }
         else
---
+++ Assets/VehicleSpawnMenu.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VehicleSpawnMenu : MonoBehaviour
+{
+    public GameObject spawnObject;
+
+    public TMPro.TMP_Text vehicleName;
+
+
+    public GameObject customizeObject;
+
+    public Image bodyColor;
+    public Image accentColor;
+    public Image tireColor;
+
+    public TMPro.TMP_Text bodyText;
+    public TMPro.TMP_Text accentText;
+    public TMPro.TMP_Text tireText;
+
+
+    public GameObject colorObject;
+
+    public TMPro.TMP_Text colorType;
+
+    public Slider rSlider;
+    public Slider gSlider;
+    public Slider bSlider;
+
+    public Image sliderColor;
+
+
+
+}
